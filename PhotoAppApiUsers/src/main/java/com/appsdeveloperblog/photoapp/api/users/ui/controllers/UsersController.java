91e0830846368a4db42d53120c4532dfdb2f3bbd
eduardo <ed.ser.cab@gmail.com>
2020-07-05 16:47:14 -0500

    Create a new controller method for creating a new user "createUser"
    Create a new class to contain the request body and anotate it with
    @RequestBody

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UsersController.java
@@ -1,8 +1,12 @@
 package com.appsdeveloperblog.photoapp.api.users.ui.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.appsdeveloperblog.photoapp.api.users.ui.model.CreateUserRequestModel;
 
 @RestController
 @RequestMapping("/users")
@@ -11,6 +15,11 @@ public class UsersController {
 	@GetMapping("/status/check")
 	public String status() {
 		return "Working";
+	}
+	
+	@PostMapping
+	public String createUser(@RequestBody CreateUserRequestModel userDetails) {
+		return "Create User methos called";
 	}
 
 }
