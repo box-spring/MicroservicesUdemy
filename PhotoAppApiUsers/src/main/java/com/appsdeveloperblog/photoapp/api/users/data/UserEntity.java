4e36af738531c570b421535bffd4e1fb3fbb7deb
eduardo <ed.ser.cab@gmail.com>
2020-07-10 21:57:48 -0500

    Add support for encrypting password BCryptEncoder and accept requests only from Gateway

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/UserEntity.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/data/UserEntity.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="users")
+@Table(name="USERS")
 public class UserEntity implements Serializable {
 
 	private static final long serialVersionUID = -2731425678149216053L;
--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UsersServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UsersServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.appsdeveloperblog.photoapp.api.users.data.UserEntity;
@@ -15,21 +16,23 @@ import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
 public class UsersServiceImpl implements UsersService {
 	
 	UsersRepository usersRepository;
+	BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	@Autowired
-	UsersServiceImpl(UsersRepository usersRepository){
+	UsersServiceImpl(UsersRepository usersRepository, BCryptPasswordEncoder bCryptPasswordEncoder){
 		this.usersRepository = usersRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 
 	@Override
 	public UserDto createUser(UserDto userDetails) {
 		userDetails.setUserId(UUID.randomUUID().toString());
+		userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
 		
 		ModelMapper mapper = new ModelMapper();
 		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		
 		UserEntity userEntity = mapper.map(userDetails, UserEntity.class);
-		userEntity.setEncryptedPassword("test");
 		
 		usersRepository.save(userEntity);
 		
